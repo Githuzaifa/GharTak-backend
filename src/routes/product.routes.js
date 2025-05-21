6315f85dc86ce2344aebc21fadf8a1fb682b031d
Githuzaifa <98671495+Githuzaifa@users.noreply.github.com>
2025-05-21 14:06:08 +0500

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -16,38 +16,30 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
 // Public routes
-router.get("/",verifyJWT, getAllProducts);
-router.get("/:productId",verifyJWT, getProductById);
-router.get("/category/:category",verifyJWT, getProductsByCategory);
+router.get("/",getAllProducts);
+router.get("/:productId",getProductById);
+router.get("/category/:category",getProductsByCategory);
 
 // Admin-only routes
 router.post(
   "/",
-  verifyJWT,
-  verifyAdmin,
   upload.single("image"),
   createProduct
 );
 
 router.patch(
   "/:productId",
-  verifyJWT,
-  verifyAdmin,
   upload.single("image"),
   updateProduct
 );
 
 router.delete(
   "/:productId",
-  verifyJWT,
-  verifyAdmin,
   deleteProduct
 );
 
 router.patch(
   "/stock/:productId",
-  verifyJWT,
-  verifyAdmin,
   updateStock
 );
 
