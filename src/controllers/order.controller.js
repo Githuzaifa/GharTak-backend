9683dc721ffc7aa00ef9fd889d54fc718b968e6f
Githuzaifa <98671495+Githuzaifa@users.noreply.github.com>
2025-06-12 05:14:08 +0500

    Update order.controller.js

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -43,7 +43,7 @@ const placeOrder = asyncHandler(async (req, res) => {
     }
 
     const Model = itemType === "Product" ? Product : Service;
-    const dbItem = await Model.findOne({ _id: new mongoose.Types.ObjectId(itemId) });
+    const dbItem = await Model.findOne({ _id: itemId });
     if (!dbItem) throw new apiError(404, `${itemType} not found`);
 
     // Check stock for products
