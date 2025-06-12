a3ba14c5edfb9444e4ecbf2484556d811556e96d
Githuzaifa <98671495+Githuzaifa@users.noreply.github.com>
2025-06-12 15:50:52 +0500

    Update product.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    _id: { type: mongoose.Schema.Types.ObjectId },
     name: {
       type: String,
       required: true,
