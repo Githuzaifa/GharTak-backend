2ed44ae1706d8b2ded661c943e2e9dcb3693b24d
Githuzaifa <98671495+Githuzaifa@users.noreply.github.com>
2025-06-12 19:20:43 +0500

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,20 +1,6 @@
+// multer.config.js
 import multer from "multer";
-// import fs from "fs";
 
-const tempDir = "../public/temp";
-
-// // Ensure the directory exists before saving files
-// if (!fs.existsSync(tempDir)) {
-//   fs.mkdirSync(tempDir, { recursive: true });
-// }
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, tempDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+const storage = multer.memoryStorage(); // file will be available in req.file.buffer
 
 export const upload = multer({ storage });
